29e02e63eddbedc0ff25a61e1e8627107179cf02
unknown <thorirv15@ru.is>
2019-10-08 23:03:52 +0000

    Button and textfield have been added.
    A textfield and a button below have been added to the UI and are
    connected. This is the final setup of the application and the first
    version of the app. It has a raw structure to build on and a good
    checkpoint for future development, until next version is pushed to the
    master branch.

--- whats_the_weather/lib/forcast_list_manager.dart
+++ whats_the_weather/lib/forcast_list_manager.dart
@@ -12,14 +12,47 @@ class ForcastListManager extends StatefulWidget {
 
 class _ForcastListManagerState extends State<ForcastListManager> {
   final List<String> _forcasts = ['First day', 'Second day'];
-  
+  String _inputValue = '';
+
+  void onPressed() {
+    // When the button is pressed,
+    // the API should be called to fetch data.
+    print('the input text is: $_inputValue');
+  }
+
+  // State of the widget is set when text
+  // is changed in the textfield.
+  void onChanged(String inputValue) {
+    setState(() {
+      _inputValue = inputValue;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: 1,
       itemBuilder: (BuildContext context, int index) {
         return Container(
-            child: Forcasts(_forcasts)
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(35.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), 
+                      labelText: 'Enter a city'),
+                      onChanged: (String value) {onChanged(value);},
+                ),
+              ),
+              RaisedButton(
+                child: Text('Find'),
+                onPressed: () { onPressed(); },
+              ),
+              Forcasts(_forcasts)
+            ],
+          ),
         );
       },
     );
