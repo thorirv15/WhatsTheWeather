27aac5494a2655dbd4b66d7eff14a503a324649b
unknown <thorirv15@ru.is>
2019-10-10 18:18:11 +0000

    Cleaning and fixing codebase.
    Cleaning, fixing and commenting code, here and there. Before pushing to
    dev branch.

--- whats_the_weather/lib/forcast_list_manager.dart
+++ whats_the_weather/lib/forcast_list_manager.dart
@@ -1,9 +1,10 @@
+// Package imports.
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-
+// Internal imports.
 import './forcasts.dart';
 
 class ForcastListManager extends StatefulWidget {
@@ -14,11 +15,13 @@ class ForcastListManager extends StatefulWidget {
 }
 
 class _ForcastListManagerState extends State<ForcastListManager> {
-  final String _weatherAPIKey = '936547b0cfmsh007bf695be676f6p118acejsn7e2010b5d570';
-  final String _baseAPIurl = 'https://community-open-weather-map.p.rapidapi.com/forecast';
-  String _inputCityValue = '', _inputCoordinatesValues = '';
+  final String _weatherAPIKey =
+      '936547b0cfmsh007bf695be676f6p118acejsn7e2010b5d570';
+  final String _baseAPIurl =
+      'https://community-open-weather-map.p.rapidapi.com/forecast';
   List _forcastData = [];
-  String _city = '', _country = '';
+  String _inputCityValue = '', _inputCoordinatesValues = '';
+  String _todayTitle = '';
 
   @override
   void initState() {
@@ -29,26 +32,23 @@ class _ForcastListManagerState extends State<ForcastListManager> {
     // When the button is pressed,
     // the API should be called to fetch data.
     String queryString = '';
-    if(searchType == 'city') {
+    if (searchType == 'city') {
       queryString = 'q=' + _inputCityValue;
-    }
-    else if(searchType == 'coordinates') {
+    } else if (searchType == 'coordinates') {
       var splitted = _inputCoordinatesValues.split(",");
-      queryString = 'lat=' + splitted[0] + '&lon=' + splitted[1];    
+      queryString = 'lat=' + splitted[0] + '&lon=' + splitted[1];
     }
 
     fetchForcastData(queryString);
-    
   }
 
   // State of the widget is set when text
   // is changed in the textfield.
   void onChanged(String searchType, String inputValue) {
     setState(() {
-      if(searchType == 'city') {
+      if (searchType == 'city') {
         _inputCityValue = inputValue;
-      }
-      else if(searchType == 'coordinates') {
+      } else if (searchType == 'coordinates') {
         _inputCoordinatesValues = inputValue;
       }
     });
@@ -57,19 +57,20 @@ class _ForcastListManagerState extends State<ForcastListManager> {
   // Construct final list to display.
   void constructFinalList(List detailedForcastData, List forcastData) {
     for (int i = 0; i < forcastData.length; i++) {
-      DateTime date = DateTime.fromMillisecondsSinceEpoch(forcastData[i]["dt"] * 1000);
-      forcastData[i]["detail"] = []; 
+      DateTime date =
+          DateTime.fromMillisecondsSinceEpoch(forcastData[i]["dt"] * 1000);
+      forcastData[i]["detail"] = [];
 
-      for(int j = 0; j < detailedForcastData.length; j++) {
-        DateTime detailDate = DateTime.fromMillisecondsSinceEpoch(detailedForcastData[j]["dt"] * 1000);
-        if(detailDate.day == date.day && detailDate.month == date.month) {
-          forcastData[i]["detail"].add(detailedForcastData[j]); 
+      for (int j = 0; j < detailedForcastData.length; j++) {
+        DateTime detailDate = DateTime.fromMillisecondsSinceEpoch(
+            detailedForcastData[j]["dt"] * 1000);
+        if (detailDate.day == date.day && detailDate.month == date.month) {
+          forcastData[i]["detail"].add(detailedForcastData[j]);
         }
       }
     }
     setState(() {
       _forcastData = forcastData;
-      
     });
   }
 
@@ -77,47 +78,40 @@ class _ForcastListManagerState extends State<ForcastListManager> {
     String url = _baseAPIurl + '/daily?' + queryParams + '&cnt=5';
 
     // Fetch forcast for the five days.
-    var res = await http.get(
-      Uri.encodeFull(url),
-      headers: {"X-RapidAPI-Key": _weatherAPIKey}
-    );
+    var res = await http
+        .get(Uri.encodeFull(url), headers: {"X-RapidAPI-Key": _weatherAPIKey});
 
-    if(res.statusCode == 200) {
+    if (res.statusCode == 200) {
       var extractData = json.decode(res.body);
       var forcastData = extractData["list"];
-      // _city = extractData["city"]["name"] + ", ";
-      // _country = extractData["city"]["country"];
-      
+
       List detailedForcastData = [];
       url = _baseAPIurl + '?' + queryParams;
-   
+
       // If a forcast for the five days was found,
       // fetch a detailed forcast for each day.
-      var detailedRes = await http.get(
-        Uri.encodeFull(url),
-        headers: {"X-RapidAPI-Key": _weatherAPIKey}
-      );
+      var detailedRes = await http.get(Uri.encodeFull(url),
+          headers: {"X-RapidAPI-Key": _weatherAPIKey});
 
-      if(detailedRes.statusCode == 200) {
+      if (detailedRes.statusCode == 200) {
         var extractData = json.decode(detailedRes.body);
         detailedForcastData = extractData["list"];
 
         setState(() {
-          _city = extractData["city"]["name"] + ", ";
-          _country = extractData["city"]["country"];
+          _todayTitle = 'Weather today in ' +
+              extractData["city"]["name"] +
+              ", " +
+              extractData["city"]["country"];
         });
 
         constructFinalList(detailedForcastData, forcastData);
       }
-    }
-    else {
+    } else {
       setState(() {
         _forcastData = [];
-        _city = "";
-        _country = "";
+        _todayTitle = "";
       });
     }
-
   }
 
   @override
@@ -125,88 +119,42 @@ class _ForcastListManagerState extends State<ForcastListManager> {
     return Container(
       child: Column(
         children: <Widget>[
-          
-          Container (
+          Container(
             margin: EdgeInsets.fromLTRB(35.0, 15, 35.0, 4),
             child: TextFormField(
-              decoration: InputDecoration(
-                  hintText: 'Enter city',
-                  border: OutlineInputBorder(),
-                  
-                  suffixIcon: IconButton(
-                      icon: Icon(Icons.check),
-                      onPressed: () {
-                        onPressed('city');
-                      })),
-                  onChanged: (String value) {onChanged('city', value);}
-            ),
+                decoration: InputDecoration(
+                    hintText: 'Enter city',
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                        icon: Icon(Icons.check),
+                        onPressed: () {
+                          onPressed('city');
+                        })),
+                onChanged: (String value) {
+                  onChanged('city', value);
+                }),
           ),
-
-          Container (
-            margin: EdgeInsets.fromLTRB(35.0, 4, 35.0, 4),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  hintText: 'Enter coordinates: Lat, Long',
-                  border: OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                      icon: Icon(Icons.check),
-                      onPressed: () {
-                        onPressed('coordinates');
-                      })),
-                  onChanged: (String value) {onChanged('coordinates', value);}
-            ),
-          ),
-          
-
           Container(
             margin: EdgeInsets.fromLTRB(35.0, 4, 35.0, 4),
-            child: Text(_city + _country)
+            child: TextFormField(
+                decoration: InputDecoration(
+                    hintText: 'Enter coordinates: Lat, Long',
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                        icon: Icon(Icons.check),
+                        onPressed: () {
+                          onPressed('coordinates');
+                        })),
+                onChanged: (String value) {
+                  onChanged('coordinates', value);
+                }),
           ),
-
+          Container(
+              margin: EdgeInsets.fromLTRB(35.0, 20, 35.0, 4),
+              child: Text(_todayTitle)),
           Forcasts(_forcastData)
-
-      ],),
+        ],
+      ),
     );
-  
-    // return ListView.builder(
-    //   itemCount: 1,
-    //   itemBuilder: (BuildContext context, int index) {
-    //     return Container(
-    //       child: Column(
-    //         children: <Widget>[
-    //           Container(
-    //             padding: EdgeInsets.all(35.0),
-    //             child: TextField(
-    //               decoration: InputDecoration(
-    //                   border: OutlineInputBorder(), 
-    //                   labelText: 'Enter a city'),
-    //                   onChanged: (String value) {onChanged('city', value);}
-    //             ),
-    //           ),
-    //           RaisedButton(
-    //             child: Text('Find'),
-    //             onPressed: () { onPressed('city'); },
-    //           ),
-    //           Container(
-    //             padding: EdgeInsets.all(35.0),
-    //             child: TextField(
-    //               decoration: InputDecoration(
-    //                   border: OutlineInputBorder(), 
-    //                   labelText: 'Enter coordinates: Lat, Long'),
-    //                   onChanged: (String value) {onChanged('coordinates', value);},
-    //             ),
-    //           ),
-    //           RaisedButton(
-    //             child: Text('Find'),
-    //             onPressed: () { onPressed('coordinates'); },
-    //           ),
-    //           Text(_city),
-    //           Text(_country),
-    //           Forcasts(_weatherData)
-    //         ],
-    //       ),
-    //     );
-    //   },
-    // );
   }
 }
--- whats_the_weather/lib/forcasts.dart
+++ whats_the_weather/lib/forcasts.dart
@@ -1,68 +1,75 @@
+// Package imports.
 import 'package:flutter/material.dart';
 
 class Forcasts extends StatelessWidget {
   final List forcast;
-  final List _weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  final List _weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
   Forcasts(this.forcast);
-
-  void onTap(int index) {
-    print("=======================");
-    print(forcast[index]);
-    print("=======================");
-  }
 
   @override
   Widget build(BuildContext context) {
-    if(forcast == null) {
+    if (forcast == null) {
       return Container();
     }
-    return 
-      Expanded(
+    return Expanded(
+        child: ListView.builder(
+      itemCount: forcast.length,
+      itemBuilder: (BuildContext context, int index) {
+        DateTime currDay =
+            DateTime.fromMillisecondsSinceEpoch(forcast[index]["dt"] * 1000);
 
-          child: ListView.builder(
-            itemCount: forcast.length,
-            itemBuilder: (BuildContext context, int index) {
-              DateTime currDay = DateTime.fromMillisecondsSinceEpoch(forcast[index]["dt"] * 1000);
-            
-              // If item is the first day
-              // if(index == 0) {
-              //   return Card(
-              //     child: Column(
-              //       children: <Widget>[
-                      
-              //       ],
-              //     )
-              //   );
-              // }
-              return Card(
-                child: ExpansionTile(
-                  title: Text(_weekDays[currDay.weekday-1] + "    AVERAGE TEMP: " + forcast[index]["temp"]["day"].toString() +  "\n" + currDay.toString().split(" ")[0]),
-                  children: <Widget>[
-                    Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: forcast[index]["detail"]
-                          .map<Widget>((elem) => Card(
-                                child: Container(
-                                    child:
-                                      Text(elem["dt_txt"] + "  " + elem["weather"][0]["main"]),
-
-                                      
-                                    padding: const EdgeInsets.all(20.0)),
-                              ))
-                          .toList(),
-                    ),
-                  )   
-                  ],             
+        // If item is the current day.
+        if (index == 0) {
+          return Card(
+            margin: EdgeInsets.fromLTRB(4, 4, 4, 4),
+            child: Column(
+              children: forcast[index]["detail"]
+                  .map<Widget>((elem) => Card(
+                        child: Container(
+                            child: Text(elem["dt_txt"] +
+                                "  " +
+                                elem["weather"][0]["main"]),
+                            padding: const EdgeInsets.all(20.0)),
+                      ))
+                  .toList(),
+            ),
+          );
+        }
+        return Card(
+          child: ExpansionTile(
+            title: Text(_weekDays[currDay.weekday - 1] +
+                "    AVERAGE TEMP: " +
+                forcast[index]["temp"]["day"].toString() +
+                "\n" +
+                currDay.toString().split(" ")[0]),
+            children: <Widget>[
+              Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: forcast[index]["detail"]
+                      .map<Widget>((elem) => Card(
+                            child: Container(
+                                child: Text(elem["dt_txt"] +
+                                    "  " +
+                                    elem["weather"][0]["main"]),
+                                padding: const EdgeInsets.all(20.0)),
+                          ))
+                      .toList(),
                 ),
-              );
-            },
-          )
+              )
+            ],
+          ),
         );
-    
-  // <Widget>[
-  //                   Text(forcast[index]["detail"].length.toString())
-  //                 ],  
-
+      },
+    ));
   }
 }
