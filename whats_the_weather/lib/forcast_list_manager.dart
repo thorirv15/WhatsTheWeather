479237c5f870a843d3dce232ab08f6c764e55b5b
unknown <thorirv15@ru.is>
2019-10-10 16:09:49 +0000

    Weather displayed for the next 5 days with detail.
    Weather is now displayd for the next five days with the possibility to
    press each day for more detail. The UI has been updated to meet the
    commands, to have the correct widgets and in the right order to make
    this happen. The next step is to display only the first day and the rest
    below.

--- whats_the_weather/lib/forcast_list_manager.dart
+++ whats_the_weather/lib/forcast_list_manager.dart
@@ -14,10 +14,10 @@ class ForcastListManager extends StatefulWidget {
 }
 
 class _ForcastListManagerState extends State<ForcastListManager> {
-  final String _weatherAPIKey = '&appid=714eba573f46406d2d6e1d6da46538cf';
-  final String _baseAPIurl = 'http://api.openweathermap.org/data/2.5/forecast?';
+  final String _weatherAPIKey = '936547b0cfmsh007bf695be676f6p118acejsn7e2010b5d570';
+  final String _baseAPIurl = 'https://community-open-weather-map.p.rapidapi.com/forecast';
   String _inputCityValue = '', _inputCoordinatesValues = '';
-  List _weatherData = [];
+  List _forcastData = [];
   String _city = '', _country = '';
 
   @override
@@ -37,7 +37,7 @@ class _ForcastListManagerState extends State<ForcastListManager> {
       queryString = 'lat=' + splitted[0] + '&lon=' + splitted[1];    
     }
 
-    fetchWeatherData(queryString);
+    fetchForcastData(queryString);
     
   }
 
@@ -54,26 +54,65 @@ class _ForcastListManagerState extends State<ForcastListManager> {
     });
   }
 
-  Future fetchWeatherData(String queryParams) async {
-    print(_baseAPIurl);
-    String url = _baseAPIurl + queryParams + _weatherAPIKey;
-    print(url);
+  // Construct final list to display.
+  void constructFinalList(List detailedForcastData, List forcastData) {
+    for (int i = 0; i < forcastData.length; i++) {
+      DateTime date = DateTime.fromMillisecondsSinceEpoch(forcastData[i]["dt"] * 1000);
+      forcastData[i]["detail"] = []; 
+
+      for(int j = 0; j < detailedForcastData.length; j++) {
+        DateTime detailDate = DateTime.fromMillisecondsSinceEpoch(detailedForcastData[j]["dt"] * 1000);
+        if(detailDate.day == date.day && detailDate.month == date.month) {
+          forcastData[i]["detail"].add(detailedForcastData[j]); 
+        }
+      }
+    }
+    setState(() {
+      _forcastData = forcastData;
+      
+    });
+  }
+
+  Future fetchForcastData(String queryParams) async {
+    String url = _baseAPIurl + '/daily?' + queryParams + '&cnt=5';
+
+    // Fetch forcast for the five days.
     var res = await http.get(
       Uri.encodeFull(url),
-      headers: {"Accept": "application/json"}
+      headers: {"X-RapidAPI-Key": _weatherAPIKey}
     );
 
     if(res.statusCode == 200) {
+      var extractData = json.decode(res.body);
+      var forcastData = extractData["list"];
+      // _city = extractData["city"]["name"] + ", ";
+      // _country = extractData["city"]["country"];
+      
+      List detailedForcastData = [];
+      url = _baseAPIurl + '?' + queryParams;
+   
+      // If a forcast for the five days was found,
+      // fetch a detailed forcast for each day.
+      var detailedRes = await http.get(
+        Uri.encodeFull(url),
+        headers: {"X-RapidAPI-Key": _weatherAPIKey}
+      );
+
+      if(detailedRes.statusCode == 200) {
+        var extractData = json.decode(detailedRes.body);
+        detailedForcastData = extractData["list"];
+
         setState(() {
-          var extractData = json.decode(res.body);
-          _weatherData = extractData["list"];
-          _city = extractData["city"]["name"];
+          _city = extractData["city"]["name"] + ", ";
           _country = extractData["city"]["country"];
-      });
+        });
+
+        constructFinalList(detailedForcastData, forcastData);
+      }
     }
     else {
       setState(() {
-        _weatherData = [];
+        _forcastData = [];
         _city = "";
         _country = "";
       });
@@ -83,45 +122,91 @@ class _ForcastListManagerState extends State<ForcastListManager> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 1,
-      itemBuilder: (BuildContext context, int index) {
-        return Container(
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(35.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), 
-                      labelText: 'Enter a city'),
-                      onChanged: (String value) {onChanged('city', value);}
-                ),
-              ),
-              RaisedButton(
-                child: Text('Find'),
-                onPressed: () { onPressed('city'); },
-              ),
-              Container(
-                padding: EdgeInsets.all(35.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), 
-                      labelText: 'Enter coordinates: Lat, Long'),
-                      onChanged: (String value) {onChanged('coordinates', value);},
-                ),
-              ),
-              RaisedButton(
-                child: Text('Find'),
-                onPressed: () { onPressed('coordinates'); },
-              ),
-              Text(_city),
-              Text(_country),
-              Forcasts(_weatherData)
-            ],
+    return Container(
+      child: Column(
+        children: <Widget>[
+          
+          Container (
+            margin: EdgeInsets.fromLTRB(35.0, 15, 35.0, 4),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  hintText: 'Enter city',
+                  border: OutlineInputBorder(),
+                  
+                  suffixIcon: IconButton(
+                      icon: Icon(Icons.check),
+                      onPressed: () {
+                        onPressed('city');
+                      })),
+                  onChanged: (String value) {onChanged('city', value);}
+            ),
           ),
-        );
-      },
+
+          Container (
+            margin: EdgeInsets.fromLTRB(35.0, 4, 35.0, 4),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  hintText: 'Enter coordinates: Lat, Long',
+                  border: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                      icon: Icon(Icons.check),
+                      onPressed: () {
+                        onPressed('coordinates');
+                      })),
+                  onChanged: (String value) {onChanged('coordinates', value);}
+            ),
+          ),
+          
+
+          Container(
+            margin: EdgeInsets.fromLTRB(35.0, 4, 35.0, 4),
+            child: Text(_city + _country)
+          ),
+
+          Forcasts(_forcastData)
+
+      ],),
     );
+  
+    // return ListView.builder(
+    //   itemCount: 1,
+    //   itemBuilder: (BuildContext context, int index) {
+    //     return Container(
+    //       child: Column(
+    //         children: <Widget>[
+    //           Container(
+    //             padding: EdgeInsets.all(35.0),
+    //             child: TextField(
+    //               decoration: InputDecoration(
+    //                   border: OutlineInputBorder(), 
+    //                   labelText: 'Enter a city'),
+    //                   onChanged: (String value) {onChanged('city', value);}
+    //             ),
+    //           ),
+    //           RaisedButton(
+    //             child: Text('Find'),
+    //             onPressed: () { onPressed('city'); },
+    //           ),
+    //           Container(
+    //             padding: EdgeInsets.all(35.0),
+    //             child: TextField(
+    //               decoration: InputDecoration(
+    //                   border: OutlineInputBorder(), 
+    //                   labelText: 'Enter coordinates: Lat, Long'),
+    //                   onChanged: (String value) {onChanged('coordinates', value);},
+    //             ),
+    //           ),
+    //           RaisedButton(
+    //             child: Text('Find'),
+    //             onPressed: () { onPressed('coordinates'); },
+    //           ),
+    //           Text(_city),
+    //           Text(_country),
+    //           Forcasts(_weatherData)
+    //         ],
+    //       ),
+    //     );
+    //   },
+    // );
   }
 }
