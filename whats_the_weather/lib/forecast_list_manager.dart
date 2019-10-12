1206f8018d07654fcc908ce2ae33b4a09fae3652
unknown <thorirv15@ru.is>
2019-10-12 23:04:09 +0000

    Refactoring code and more detailed comments.
    Code has been structured in an organized way. The widgets are now
    constructed outside of the build method for all classes where they are
    called opon by the build method.
    More detailed and better comments on multiple places.

---
+++ whats_the_weather/lib/forecast_list_manager.dart
@@ -0,0 +1,262 @@
+// Package imports.
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+
+// Internal imports.
+import './forecasts.dart';
+
+class ForecastListManager extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ForecastListManagerState();
+}
+
+class _ForecastListManagerState extends State<ForecastListManager> {
+  // Url and key for fetching weather data.
+  final String _weatherAPIKey =
+      '936547b0cfmsh007bf695be676f6p118acejsn7e2010b5d570';
+  final String _baseAPIurl =
+      'https://community-open-weather-map.p.rapidapi.com/';
+
+  // Vital variables to keep track of state.
+  String _inputCityValue = '', _inputCoordinatesValues = '';
+  List _forecastData = [];
+  bool _cacheIsClear = true;
+
+  @override
+  void initState() {
+    // Check if the latest search is stored in cache
+    // and should be displayed.
+    readFromSharedPreferences();
+    super.initState();
+  }
+
+  // Reads from cache. Both the last input and the timestamp
+  // for that input to check if data should be removed.
+  Future readFromSharedPreferences() async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    final timeStampKey = 'timestamp_key';
+
+    // Start by fetching the timestamp.
+    final storedTimeStampStr = sharedPrefs.getString(timeStampKey);
+    if (storedTimeStampStr == null) {
+      return;
+    }
+    DateTime storedTimeStamp = DateTime.parse(storedTimeStampStr);
+    DateTime currentTimeStamp = DateTime.now();
+
+    // If more than 12 hours from stored timestamp,
+    // cache will be cleared.
+    if (currentTimeStamp.difference(storedTimeStamp).inHours > 12) {
+      sharedPrefs.clear();
+      setState(() => _cacheIsClear = true);
+      return;
+    }
+
+    // The most recent weather forecast for the latest
+    // search input in the application is fetched if timestamp is valid.
+    final searchKey = 'search_key';
+    final val = sharedPrefs.getString(searchKey);
+    if (val != null) {
+      _cacheIsClear = false;
+      fetchforecastData(val);
+    }
+  }
+
+  // Save to cache. Latest search input is stored along
+  // with a timestamp of the search so that the user will
+  // get the newest most recent forecast based on previous input.
+  Future saveToSharedPreferences(String inputSearchKey) async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    final searchKey = 'search_key';
+    final timeStampKey = 'timestamp_key';
+
+    // Cache data.
+    await sharedPrefs.setString(timeStampKey, DateTime.now().toString());
+    await sharedPrefs.setString(searchKey, inputSearchKey);
+    setState(() => _cacheIsClear = false);
+  }
+
+  void onPressed(String searchType) {
+    // When the button is pressed,
+    // the API should be called to fetch data.
+    String queryString = '';
+    if (searchType == 'city') {
+      queryString = 'q=' + _inputCityValue;
+    } else if (searchType == 'coordinates') {
+      var splitted = _inputCoordinatesValues.split(',');
+      queryString = 'lat=' + splitted[0] + '&lon=' + splitted[1];
+    }
+
+    // Store search in cache for maximum 12 hours.
+    saveToSharedPreferences(queryString);
+    // Then fetch forecast data to display.
+    fetchforecastData(queryString);
+  }
+
+  // State of the input values are set when text
+  // is changed in either textfield.
+  void onChanged(String searchType, String inputValue) {
+    setState(() {
+      if (searchType == 'city') {
+        _inputCityValue = inputValue;
+      } else if (searchType == 'coordinates') {
+        _inputCoordinatesValues = inputValue;
+      }
+    });
+  }
+
+  Future clearCache() async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    sharedPrefs.clear();
+    setState(() => _cacheIsClear = true);
+  }
+
+  // Construct final list to display.
+  void constructFinalList(
+      List detailedforecastData, List forecastData, dynamic forecastNowData) {
+    // Iterate through the 5 day forecast and for each day,
+    // add a detailed forecast for that day.
+    for (int i = 0; i < forecastData.length; i++) {
+      DateTime date =
+          DateTime.fromMillisecondsSinceEpoch(forecastData[i]['dt'] * 1000);
+
+      // Create a new key for the detail part of the data.
+      // A list of the 5 days in more detail.
+      forecastData[i]['detail'] = [];
+
+      // If it is the first day, add a key for the
+      // present weather data.
+      if (i == 0) {
+        forecastData[i]['todayForecast'] = forecastNowData;
+      }
+
+      for (int j = 0; j < detailedforecastData.length; j++) {
+        DateTime detailDate = DateTime.fromMillisecondsSinceEpoch(
+            detailedforecastData[j]['dt'] * 1000);
+
+        if (detailDate.day == date.day && detailDate.month == date.month) {
+          forecastData[i]['detail'].add(detailedforecastData[j]);
+        }
+      }
+    }
+    // The view will now be updated with the forecast.
+    setState(() => _forecastData = forecastData);
+  }
+
+  // Fetch the forecasts from the weather API for the present,
+  // next five days and the details for the five days.
+  // If one of the request fails, no data will be set.
+  Future fetchforecastData(String queryParams) async {
+    bool fetchedAllData = false;
+    String url = _baseAPIurl + 'weather?' + queryParams;
+
+    // Start by fetching the forecast at this moment.
+    var forecastNowRes = await http
+        .get(Uri.encodeFull(url), headers: {'X-RapidAPI-Key': _weatherAPIKey});
+
+    if (forecastNowRes.statusCode == 200) {
+      var extractData = json.decode(forecastNowRes.body);
+      var forecastNowData = extractData;
+
+      // Fetch forecast for the next five days.
+      url = _baseAPIurl + 'forecast/daily?' + queryParams + '&cnt=5';
+
+      var forecast5dayRes = await http.get(Uri.encodeFull(url),
+          headers: {'X-RapidAPI-Key': _weatherAPIKey});
+
+      if (forecast5dayRes.statusCode == 200) {
+        extractData = json.decode(forecast5dayRes.body);
+        var forecast5dayData = extractData['list'];
+
+        // Finally fetch a detailed forecast for each day.
+        url = _baseAPIurl + 'forecast?' + queryParams;
+
+        var detailedRes = await http.get(Uri.encodeFull(url),
+            headers: {'X-RapidAPI-Key': _weatherAPIKey});
+
+        if (detailedRes.statusCode == 200) {
+          fetchedAllData = true;
+          var extractData = json.decode(detailedRes.body);
+          var detailedforecastData = extractData['list'];
+
+          // When all data needed has been fetched, the final list
+          // will be constructed and state will be set with that list.
+          constructFinalList(
+              detailedforecastData, forecast5dayData, forecastNowData);
+        }
+      }
+    }
+
+    // If some data was not fetched. The state will be
+    // set as if nothing was found.
+    if (!fetchedAllData) {
+      setState(() => _forecastData = []);
+    }
+  }
+
+  // The body part of the application is wrapped in a container
+  // which has a column lined with containers for the refresh cache
+  // button and the input fields. The forecasts ar displayd in a card widget.
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          refreshCacheButton(),
+          inputCityField(),
+          inputCoordinatesField(),
+          Forecasts(_forecastData)
+        ],
+      ),
+    );
+  }
+
+  // Below are the implementations of the widgets.
+  @widget
+  Widget refreshCacheButton() => Container(
+      margin: EdgeInsets.fromLTRB(35.0, 15, 35.0, 4),
+      child: IgnorePointer(
+          ignoring: _cacheIsClear,
+          child: RaisedButton(
+              padding: EdgeInsets.all(8.0),
+              child: Text('Refresh Cache'),
+              onPressed: () => clearCache())));
+
+  @widget
+  Widget inputCityField() => Container(
+        margin: EdgeInsets.fromLTRB(35.0, 15, 35.0, 4),
+        child: TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Enter city',
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                    icon: Icon(Icons.check),
+                    onPressed: () {
+                      onPressed('city');
+                    })),
+            onChanged: (String value) {
+              onChanged('city', value);
+            }),
+      );
+
+  @widget
+  Widget inputCoordinatesField() => Container(
+        margin: EdgeInsets.fromLTRB(35.0, 4, 35.0, 4),
+        child: TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Enter coordinates: Lat, Long',
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                    icon: Icon(Icons.check),
+                    onPressed: () {
+                      onPressed('coordinates');
+                    })),
+            onChanged: (String value) {
+              onChanged('coordinates', value);
+            }),
+      );
+}
---
+++ whats_the_weather/lib/forecasts.dart
@@ -0,0 +1,167 @@
+// Package imports.
+import 'package:flutter/material.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+
+// Internal imports.
+import './forecast_details.dart';
+
+class Forecasts extends StatelessWidget {
+  final List forecast;
+  final List _weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  final List _months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec'
+  ];
+  Forecasts(this.forecast);
+
+  String kelvinToCelciusStr(var tempInKelvin) =>
+      ((tempInKelvin - 272.15).round()).toString() + '°C';
+
+  // The forcast display is in an expanded widget which inhlds
+  // a listview where the first item in the list (today) is displayed
+  // in a special card, where the detail information will always be
+  // displayed along with more details. The next 4 days are stored in
+  // expansion tiles which can be clicked to see details for that day.
+  @override
+  Widget build(BuildContext context) {
+    if (forecast == null) {
+      return Container();
+    }
+    return Expanded(
+        child: ListView.builder(
+      itemCount: forecast.length,
+      itemBuilder: (BuildContext context, int index) {
+        DateTime currDay =
+            DateTime.fromMillisecondsSinceEpoch(forecast[index]['dt'] * 1000);
+        List<String> dateStrings = currDay.toString().split(' ')[0].split('-');
+
+        // If item is the current day.
+        if (index == 0) {
+          return Card(
+            margin: EdgeInsets.fromLTRB(4, 4, 4, 4),
+            child: Column(
+              children: <Widget>[
+                currentLocationTitle(index),
+                infoRow(currDay, dateStrings, index),
+                ForecastDetails(forecast[index]['detail'])
+              ],
+            ),
+          );
+        } else {
+          return Card(
+            child: ExpansionTile(
+                leading: dateContainer(currDay, dateStrings, 0, 8),
+                title: tempExpansionTileContainer(index),
+                children: <Widget>[ForecastDetails(forecast[index]['detail'])],
+                trailing: highLowTempContainer(
+                    index,
+                    forecast[index]['temp']['max'],
+                    forecast[index]['temp']['min'],
+                    0)),
+          );
+        }
+      },
+    ));
+  }
+
+  // Below are the implementations of the widgets.
+  @widget
+  Widget currentLocationTitle(int index) => Container(
+        margin: EdgeInsets.fromLTRB(0, 7, 0, 0),
+        child: Text(
+            'Weather now in ' +
+                forecast[index]['todayForecast']['name'] +
+                ', ' +
+                forecast[index]['todayForecast']['sys']['country'],
+            style: TextStyle(fontSize: 20.0)),
+      );
+
+  @widget
+  Widget infoRow(DateTime currDay, List<String> dateStrings, int index) => Row(
+        children: <Widget>[
+          dateContainer(currDay, dateStrings, 10, 17),
+          tempNowContainer(index),
+          weatherDescriptionNowContainer(index),
+          highLowTempContainer(
+              index,
+              forecast[index]['todayForecast']['main']['temp_max'],
+              forecast[index]['todayForecast']['main']['temp_min'],
+              45)
+        ],
+      );
+
+  @widget
+  Widget dateContainer(DateTime currDay, List<String> dateStrings,
+          double marginL, double marginT) =>
+      Container(
+          margin: EdgeInsets.fromLTRB(marginL, marginT, 0, 0),
+          child: Column(
+            children: <Widget>[
+              Text(_weekDays[currDay.weekday - 1],
+                  style: TextStyle(fontSize: 16.0)),
+              Text(dateStrings[2] +
+                  ' ' +
+                  _months[int.parse(dateStrings[1]) - 1] +
+                  ' ' +
+                  dateStrings[0])
+            ],
+          ));
+
+  @widget
+  Widget tempNowContainer(int index) => Container(
+      margin: EdgeInsets.fromLTRB(40, 5, 0, 0),
+      child: Text(
+          kelvinToCelciusStr(forecast[index]['todayForecast']['main']['temp']),
+          style: TextStyle(fontSize: 20.0)));
+
+  @widget
+  Widget weatherDescriptionNowContainer(int index) => Container(
+      margin: EdgeInsets.fromLTRB(35, 17, 0, 0),
+      child: Column(
+        children: <Widget>[
+          Text(forecast[index]['todayForecast']['weather'][0]['main'],
+              style: TextStyle(fontSize: 20.0)),
+          Text(
+              'Humidity: ' + forecast[index]['todayForecast']['main']['humidity'].toString() + '%',
+              style: TextStyle(fontSize: 12.0))
+        ],
+      ));
+
+  @widget
+  Widget highLowTempContainer(int index, var max, var min, double marginL) =>
+      Container(
+          margin: EdgeInsets.fromLTRB(marginL, 13, 0, 0),
+          child: Column(
+            children: <Widget>[
+              Text('High: ' + kelvinToCelciusStr(max),
+                  style: TextStyle(fontSize: 12.0)),
+              Text('Low: ' + kelvinToCelciusStr(min),
+                  style: TextStyle(
+                    fontSize: 12.0,
+                  )),
+            ],
+          ));
+
+  @widget
+  Widget tempExpansionTileContainer(int index) => Container(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+                flex: 2,
+                child: Text('Average: ' +
+                    kelvinToCelciusStr(forecast[index]['temp']['day']))),
+            Expanded(child: Text(forecast[index]['weather'][0]['main'])),
+          ],
+        ),
+      );
+}
